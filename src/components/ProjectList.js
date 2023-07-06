48ba3c0d25964d25479a44564bed42a19a225632
Brandi L Mason <masonbrandi66@gmail.com>
2023-07-06 13:48:30 -0600

    lab completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,24 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  // console.log(ProjectItem);
+
+  const proj = projects.map((proj1) => {
+    // console.log(proj1) 
+    // return 1
+    // return <div>{proj1.name}</div>
+    return <ProjectItem key={proj1.id} id={proj1.id} name={proj1.name} about={proj1.about} technologies={proj1.technologies}/>
+  })
+
+  // console.log(proj)
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */}
+        {/* <ProjectItem id={projects.id} name={projects.name} about={projects.about} technologies={projects.technologies}/> */}
+        {proj}
+      </div>
     </div>
   );
 }
